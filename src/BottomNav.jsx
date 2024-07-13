c28f9b097aec12e4072543e1543ae6f31a34b998
mdurando <151706154+mdurandop@users.noreply.github.com>
2024-07-13 15:14:10 -0500

    first commit

---
+++ src/BottomNav.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const BottomNav = () => {
+  return (
+    <div className='flex max-w-full bottom-0 gap-4 absolute'>
+      <div className='flex flex-col justify-center items-center cursor-pointer'>
+        <img src="/assets/home.svg" alt="Home Icon" className='w-9'/>
+        <p className='text-xs'>Home</p>
+      </div>
+      <div className='flex flex-col justify-center items-center cursor-pointer'>
+        <img src="/assets/transaction.svg" alt="Home Icon" className='w-9'/>
+        <p className='p-opacity text-xs'>Transaction</p>
+      </div>
+      <div className='flex flex-col justify-center items-center cursor-pointer'>
+        <img src="/assets/Add.svg" alt="Home Icon" className='w-12 mb-6'/>
+      </div>
+      <div className='flex flex-col justify-center items-center cursor-pointer'>
+        <img src="/assets/pie-chart.svg" alt="Home Icon" className='w-9'/>
+        <p className='p-opacity text-xs'>Budget</p>
+      </div>
+      <div className='flex flex-col justify-center items-center cursor-pointer'>
+        <img src="/assets/user.svg" alt="Home Icon" className='w-9'/>
+        <p className='p-opacity text-xs'>Profile</p>
+      </div>
+    </div>
+  );
+};
+
+export default BottomNav;
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Container from './Container'
+import BottomNav from './BottomNav'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Container>
+
+      
+      <BottomNav />
+    </Container>
+  </React.StrictMode>,
+)
